b17455c59c8c4f7587a9dd868236a146cf311724
Aubameyang17 <95569587+Aubameyang17@users.noreply.github.com>
2025-05-20 22:43:49 +0300

    fontanka end

--- Fontankaparser.py
+++ Fontankaparser.py
@@ -1,50 +1,66 @@
+import psycopg2
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import traceback
 
+from urllib3.exceptions import ReadTimeoutError
+
 from OpenRouterApi import summorize_text
+
+conn = psycopg2.connect(dbname="ItmosnikIko", host="127.0.0.1", user="Alex", password="alex")
+cursor = conn.cursor()
 
 
 def get_links(links, titles, times, imgs):
     options_chrome = webdriver.ChromeOptions()
-    driverpobeda = webdriver.Chrome(options=options_chrome)
+    options_chrome.add_argument("--headless")
+    options_chrome.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=options_chrome)
 
     try:
         url = "https://www.fontanka.ru/realty/"
-        driverpobeda.get(url)
-        wait = WebDriverWait(driverpobeda, 20)
+        driver.get(url)
+        wait = WebDriverWait(driver, 20)
         table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "announcementList_3JPr4")))
         content = table.find_elements(By.CLASS_NAME, "wrap_DjvF8")
 
         for el in content:
-            new_link = el.find_element(By.CLASS_NAME, "header_DjvF8")
-            time = el.find_element(By.CLASS_NAME, "text_0UNFI")
+            new_link = el.find_element(By.CLASS_NAME, "header_DjvF8").get_attribute("href")
+
+            # 🔍 Проверка: есть ли такая ссылка в БД
+            cursor.execute("SELECT 1 FROM news WHERE origin_link = %s", (new_link,))
+            if cursor.fetchone():
+                continue  # Ссылка уже есть в БД
+
+            # Добавляем новую ссылку и сопутствующие данные
+            links.append(new_link)
+            titles.append(el.find_element(By.CLASS_NAME, "header_DjvF8").text)
+            times.append(el.find_element(By.CLASS_NAME, "text_0UNFI").text)
+
             try:
                 img = el.find_element(By.TAG_NAME, "img").get_attribute("src")
             except Exception:
                 img = ""
-            links.append(new_link.get_attribute("href"))
-            titles.append(new_link.text)
-            times.append(time.text)
             imgs.append(img)
-            print(f"Title: {new_link.text}\nTime: {time.text}\nImage Link: {img}\nOrigin Link: {new_link.get_attribute('href')}\n\n")
 
-    except Exception as ex:
+            print(f"🆕 Найдена новая статья: {new_link}")
+
+    except Exception:
         traceback.print_exc()
-        print("Рейсы не найдены")
+        print("⚠️ Ошибка при загрузке страницы Fontanka")
 
     finally:
-        driverpobeda.quit()  # Закрываем браузер
+        driver.quit()
 
     return links, titles, times, imgs
 
 
 def chek_news(link):
     options_chrome = webdriver.ChromeOptions()
-    # options_chrome.add_argument("--headless")
-    # options_chrome.add_argument('--no-sandbox')
+    #options_chrome.add_argument("--headless")
+    #options_chrome.add_argument('--no-sandbox')
     driverpobeda = webdriver.Chrome(options=options_chrome)
     text = ""
     if 'longreads' in link:
@@ -59,9 +75,9 @@ def chek_news(link):
                     text += el.text + " "
 
             text = summorize_text(text)
-            print(f"Text: {text}\n\n")
 
-
+        except ReadTimeoutError:
+            driverpobeda.execute_script("window.stop();")
         except Exception as ex:
             traceback.print_exc()
             print("Рейсы не найдены")
@@ -78,9 +94,9 @@ def chek_news(link):
             table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "gridContent_6I1dB")))
             content = table.find_element(By.CLASS_NAME, "articleContent_fefJj")
             text = summorize_text(content.text)
-            print(f"Text: {text}\n\n")
 
-
+        except ReadTimeoutError:
+            driverpobeda.execute_script("window.stop();")
         except Exception as ex:
             traceback.print_exc()
             print("Рейсы не найдены")
@@ -97,10 +113,26 @@ titles = []
 times = []
 imgs = []
 texts = []
-
+count = 10
 new_links, titles, times, imgs = get_links(links, titles, times, imgs)
 
-for link in new_links:
-    text = chek_news(link)
+
+for link in range(10):
+    text = chek_news(new_links[link])
     texts.append(text)
+
+
+print("Len Links: ", len(links))
+print("Len titles: ", len(titles))
+print("Len times: ", len(times))
+print("Len imgs: ", len(imgs))
+print("Len texts: ", len(texts))
+check_title = []
+for i in range(len(texts)):
+    check_title.append(titles[i])
+    parametrs = (titles[i], times[i], texts[i], imgs[i], new_links[i])
+    print(f"Link: {new_links[i]}\nTitle: {titles[i]}\nTime: {times[i]}\nImg Link{imgs[i]}\n Text: {texts[i]}\n\n")
+    cursor.execute('INSERT INTO news (title, date, text, image_link, origin_link) '
+                                               'VALUES (%s, %s, %s, %s, %s)', parametrs)
+    conn.commit()
 
